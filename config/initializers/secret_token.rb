82a0a2340a011bf0265bbb2263557e23b880450a
Nishant samel <nishants@weboniselab.com>
2014-07-30 12:14:11 +0530

    Made changes for i18n

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+I18nGemDemo::Application.config.secret_token = '1f8e41e9247a2fd7a2d7d6a99f4451b9ac3594f28fd46a4e6e4f580c777d0475ce050b46a505a5afd6f9f5fbcbe3e6ef4599ea3dc542cd4c7913e6bbeba2ba49'
